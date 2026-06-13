bf520138f2ba1c65ae6eb3ee1569766ca623c169
Lin, Xiao <xiguaxiao@foxmail.com>
2026-06-13 11:01:10 +0800

    fix(logger): 将根日志级别从DEBUG改为INFO
    调整全局默认日志输出等级，减少冗余的debug级日志输出

--- src/core/logger.py
+++ src/core/logger.py
@@ -99,7 +99,7 @@ class LogManager:
         self._queue_listener.start()
 
         root_logger = logging.getLogger()
-        root_logger.setLevel(logging.DEBUG)
+        root_logger.setLevel(logging.INFO)
         root_logger.addHandler(self._queue_handler)
 
         atexit.register(self._stop_listener)
